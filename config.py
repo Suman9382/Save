1c6c832c295ac5b341f1f857359078a54e089c9f
Suman9382 <swapan9641@yahoo.com>
2026-02-26 08:44:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -31,7 +31,7 @@ DB_NAME      = os.getenv("DB_NAME", "telegram_downloader")
 OWNER_ID     = list(map(int, os.getenv("OWNER_ID", "6814614245").split()))  # space-separated list
 STRING       = os.getenv("STRING", None)  # optional session string
 LOG_GROUP    = int(os.getenv("LOG_GROUP", "-1003759762110"))
-FORCE_SUB    = int(os.getenv("FORCE_SUB", ""))
+FORCE_SUB    = int(os.getenv("FORCE_SUB", "-1003306883946"))
 
 # ─── SECURITY KEYS ──────────────────────────────────────────────────────────────
 MASTER_KEY   = os.getenv("MASTER_KEY", "gK8HzLfT9QpViJcYeB5wRa3DmN7P2xUq")  # session encryption
@@ -77,6 +77,7 @@ P0 = {
 # ════════════════════════════════════════════════════════════════════════════════
 # ░ DEVGAGAN
 # ════════════════════════════════════════════════════════════════════════════════
+
 
 
 
